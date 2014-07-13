a392210a19f24e404095049d2090c95fc64e03de
Carly Heibel <heibelcj@rose-hulman.edu>
2014-07-13 10:18:59 -0400

    initial project version

---
+++ gen/edu/rosehulman/algweightlosstracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.rosehulman.algweightlosstracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/rosehulman/algweightlosstracker/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.rosehulman.algweightlosstracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
